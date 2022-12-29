c7778300521ebbb49c34e9e4e49bcdfdc411f152
net2devcrypto <info@a3b.io>
2022-12-29 00:06:34 -0800

    Web3 User Profiles Part1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/custom.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='backstyle'>
+  <Component {...pageProps} />
+  </div>
+  )
 }
